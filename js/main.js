76b3698236aedec893a84879d229f08f3857d386
shine1594 <panamera1594@gmail.com>
2017-11-28 14:38:07 +0900

    [IMP] implemented tab ui completely

--- js/main.js
+++ js/main.js
@@ -1,31 +1,66 @@
 
-const getData = (() => {
-  const BASE_URL = 'http://crong.codesquad.kr:8080/woowa/';
+function requestData(params) {
+  const xhttp = new XMLHttpRequest();
 
-  const urls = {
-    bestseller: BASE_URL + 'best'
-  }
-
-  const getURL = (params) => {
-    if (params.type === 'bestseller') {
-      return urls[params.type];
+  xhttp.onreadystatechange = function(evt, res) {
+    if (this.readyState == 4 && this.status == 200) {
+      const response = evt.target.response;
+      params.callback(response);
     }
+  };
+
+  xhttp.open("GET", params.url, true);
+  xhttp.send();
+}
+
+function makeListElement(data) {
+  const listElement = document.createElement('li');
+  listElement.dataset.index = data.index;
+  listElement.textContent = data.text;
+
+  return listElement;
+}
+
+function getThumbnailHTML(template, data) {
+  const keys = Object.keys(data);
+  let htmlStr = template.innerHTML;
+
+  keys.forEach((key) => {
+      htmlStr = htmlStr.replace(`{{${key}}}`, data[key]);
+  });
+
+  htmlStr = htmlStr.replace('{{n_price}}', '');
+
+  const tempNode = document.createElement('div');
+  tempNode.innerHTML = htmlStr;
+
+  const deliveryInfo = tempNode.querySelector('.wrap-delivery-info');
+  const tags = tempNode.querySelector('.thumbnail__tags');
+
+  //delivery types
+  data.delivery_type.forEach((type, index) => {
+    if (index !== 0) {
+      const separator = document.createElement('hr');
+      deliveryInfo.appendChild(separator);
+    }
+
+    const info = document.createElement('sapn');
+    info.textContent = type;
+    
+    deliveryInfo.appendChild(info);
+  });
+
+  //tag
+  if (data.badge) {
+    data.badge.forEach((tagName, index) => {
+      const tag = document.createElement('li');
+      tag.textContent = tagName;
+      tags.appendChild(tag);
+    });
   }
 
-  return (params) => {
-    const xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function(evt, res) {
-      if (this.readyState == 4 && this.status == 200) {
-        const response = evt.target.response;
-        params.callback(response);
-      }
-    };
-
-    xhttp.open("GET", getURL(params), true);
-    xhttp.send();
-  }
-})();
+  return tempNode.innerHTML;
+}
 
 function makeTabs(response) {
   const jsonData = JSON.parse(response);
@@ -33,61 +68,14 @@ function makeTabs(response) {
   const tabElement = document.querySelector('.bestseller .tabs');
   const bestItemElement = document.querySelector('#bestItemTemplate');
 
-  const makeListElement = (data) => {
-    const listElement = document.createElement('li');
-    listElement.dataset.index = data.index;
-    listElement.textContent = data.text;
-
-    return listElement;
-  };
-
-   const mappingThumbnailData = (template, obj) => {
-      const keys = Object.keys(obj);
-      let htmlStr = template.innerHTML;
-
-      keys.forEach((key) => {
-          htmlStr = htmlStr.replace(`{{${key}}}`, obj[key]);
-      });
-
-      htmlStr = htmlStr.replace('{{n_price}}', '');
-
-      const tempNode = document.createElement('div');
-      tempNode.innerHTML = htmlStr;
-
-      const deliveryInfo = tempNode.querySelector('.wrap-delivery-info');
-      const tags = tempNode.querySelector('.thumbnail__tags');
-
-      //delivery types
-      obj.delivery_type.forEach((type, index) => {
-        if (index !== 0) {
-          const separator = document.createElement('hr');
-          deliveryInfo.appendChild(separator);
-        }
-
-        const info = document.createElement('sapn');
-        info.textContent = type;
-        
-        deliveryInfo.appendChild(info);
-      });
-
-      //tag
-      if (obj.badge) {
-        obj.badge.forEach((tagName, index) => {
-          const tag = document.createElement('li');
-          tag.textContent = tagName;
-          tags.appendChild(tag);
-        });
-      }
-
-      return tempNode.innerHTML;
-  };
-
   jsonData.forEach((category, index) => {
     //1. tab 버튼 생성
     const tabButton = makeListElement({
       index: index,
       text: category.name
     });
+
+    tabElement.appendChild(tabButton);
 
     //2. ul 태그 생성
     const thumbnailList = document.createElement('ul');
@@ -97,24 +85,43 @@ function makeTabs(response) {
     //3. thumbnail dom 생성후 data 바인딩하기
     category.items.forEach((item) => {
       const thumbnailTemplate = document.querySelector('#bestItemTemplate');
-      const thumbnailHTML = mappingThumbnailData(thumbnailTemplate, item);
+      const thumbnailHTML = getThumbnailHTML(thumbnailTemplate, item);
 
       thumbnailList.innerHTML += thumbnailHTML;
     });
 
     containerElement.appendChild(thumbnailList);
+  });
 
-    //4. tab 버튼 add click event
+  //4. tab 버튼 add click event
+  const thumbnailLists = document.querySelectorAll('.thumbnails');
 
+  tabElement.addEventListener('click', (evt) => {
+    const currentIndex = tabElement.dataset.currentIndex;
+    const selectedIndex = evt.target.dataset.index;
 
+    // button active toggle
+    tabElement.children[currentIndex].classList.remove('active');
+    evt.target.classList.add('active');
+
+    // ul display toggle
+    thumbnailLists[currentIndex].style = 'display: none;';
+    thumbnailLists[selectedIndex].style = 'display: block;';
+
+    tabElement.dataset.currentIndex = selectedIndex;
   });
 
   //5 초기화 (첫 번째 버튼에 active 클래스 추가 / 첫 번째 ul "display: block")
+  tabElement.children[0].classList.add('active');
+  thumbnailLists.forEach((thumb) => thumb.style = 'display: none;');
+  thumbnailLists[0].style = 'display: block;';
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  getData({
-    type: 'bestseller',
+  const BASE_URL = 'http://crong.codesquad.kr:8080/woowa/';
+
+  requestData({
+    url: BASE_URL + 'best',
     callback: makeTabs
   });
 });
