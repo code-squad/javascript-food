d742de3c8713e16725620fd86178132bc0d25cae
shine1594 <panamera1594@gmail.com>
2017-11-28 21:11:27 +0900

    [REF] Modularized tab ui

--- js/main.js
+++ js/main.js
@@ -13,115 +13,132 @@ function requestData(params) {
   xhttp.send();
 }
 
-function makeListElement(data) {
-  const listElement = document.createElement('li');
-  listElement.dataset.index = data.index;
-  listElement.textContent = data.text;
-
-  return listElement;
+function TabMenu(selector) {
+  this.buttonsContainer = document.querySelector(selector.buttons);
+  this.contentsContainer = document.querySelector(selector.contents);
+  this.thumbnailLists = null;
+  this.buttons = [];
+  this.contents = [];
 }
 
-function getThumbnailHTML(template, data) {
-  const keys = Object.keys(data);
-  let htmlStr = template.innerHTML;
+TabMenu.prototype = {
+  constructor: TabMenu,
+  init: function(jsonStr) {
+    const data = JSON.parse(jsonStr);
 
-  keys.forEach((key) => {
-      htmlStr = htmlStr.replace(`{{${key}}}`, data[key]);
-  });
+    this.setData(data);
+    this.makeButtons();
+    this.bindClickEvent();
+    this.makeContents();
 
-  htmlStr = htmlStr.replace('{{n_price}}', '');
+    //initialize
+    this.buttonsContainer.children[0].classList.add('active');
+    this.thumbnailLists = document.querySelectorAll('.thumbnails');
+    this.thumbnailLists.forEach((thumb) => thumb.style = 'display: none;');
+    this.thumbnailLists[0].style = 'display: block;';
 
-  const tempNode = document.createElement('div');
-  tempNode.innerHTML = htmlStr;
+  },
+  setData: function(data) {
+    data.forEach((menu) => {
+      this.buttons.push({
+        id: menu.category_id,
+        text: menu.name
+      });
 
-  const deliveryInfo = tempNode.querySelector('.wrap-delivery-info');
-  const tags = tempNode.querySelector('.thumbnail__tags');
+      this.contents.push(menu.items);
+    });
+  },
+  makeButtons: function() {
+    this.buttons.forEach(((button, index) => {
+      const $button = document.createElement('li');
+      $button.dataset.index = index;
+      $button.textContent = button.text;
 
-  //delivery types
-  data.delivery_type.forEach((type, index) => {
-    if (index !== 0) {
-      const separator = document.createElement('hr');
-      deliveryInfo.appendChild(separator);
+      this.buttonsContainer.appendChild($button);
+    }));
+  },
+  makeContents: function() {
+    const $thumbnailTemplate = document.querySelector('#bestItemTemplate');
+
+    this.contents.forEach((contentItems) => {
+      const $list = document.createElement('ul');
+      $list.classList.add('thumbnails');
+
+      contentItems.forEach((item) => {
+        $list.innerHTML += this.getThumbnailHTML($thumbnailTemplate, item);
+      });
+
+      this.contentsContainer.appendChild($list);
+    });
+  },
+  getThumbnailHTML: function(template, data) {
+    const keys = Object.keys(data);
+    let htmlStr = template.innerHTML;
+
+    keys.forEach((key) => {
+        htmlStr = htmlStr.replace(`{{${key}}}`, data[key]);
+    });
+
+    htmlStr = htmlStr.replace('{{n_price}}', '');
+
+    const tempNode = document.createElement('div');
+    tempNode.innerHTML = htmlStr;
+
+    const deliveryInfo = tempNode.querySelector('.wrap-delivery-info');
+    const tags = tempNode.querySelector('.thumbnail__tags');
+
+    //delivery types
+    data.delivery_type.forEach((type, index) => {
+      if (index !== 0) {
+        const separator = document.createElement('hr');
+        deliveryInfo.appendChild(separator);
+      }
+
+      const info = document.createElement('sapn');
+      info.textContent = type;
+      
+      deliveryInfo.appendChild(info);
+    });
+
+    //tag
+    if (data.badge) {
+      data.badge.forEach((tagName, index) => {
+        const tag = document.createElement('li');
+        tag.textContent = tagName;
+        tags.appendChild(tag);
+      });
     }
 
-    const info = document.createElement('sapn');
-    info.textContent = type;
-    
-    deliveryInfo.appendChild(info);
-  });
+    return tempNode.innerHTML;
+  },
+  bindClickEvent: function() {
+    this.buttonsContainer.addEventListener('click', (evt) => {
+      const currentIndex = this.buttonsContainer.dataset.currentIndex;
+      const selectedIndex = evt.target.dataset.index;
 
-  //tag
-  if (data.badge) {
-    data.badge.forEach((tagName, index) => {
-      const tag = document.createElement('li');
-      tag.textContent = tagName;
-      tags.appendChild(tag);
+      // button active toggle
+      this.buttonsContainer.children[currentIndex].classList.remove('active');
+      evt.target.classList.add('active');
+
+      // ul display toggle
+      this.thumbnailLists[currentIndex].style = 'display: none;';
+      this.thumbnailLists[selectedIndex].style = 'display: block;';
+
+      this.buttonsContainer.dataset.currentIndex = selectedIndex;
     });
   }
-
-  return tempNode.innerHTML;
-}
-
-function makeTabs(response) {
-  const jsonData = JSON.parse(response);
-  const containerElement = document.querySelector('.bestseller > .container');
-  const tabElement = document.querySelector('.bestseller .tabs');
-  const bestItemElement = document.querySelector('#bestItemTemplate');
-
-  jsonData.forEach((category, index) => {
-    //1. tab 버튼 생성
-    const tabButton = makeListElement({
-      index: index,
-      text: category.name
-    });
-
-    tabElement.appendChild(tabButton);
-
-    //2. ul 태그 생성
-    const thumbnailList = document.createElement('ul');
-    thumbnailList.classList.add('thumbnails');
-    thumbnailList.dataset.index = index;
-    
-    //3. thumbnail dom 생성후 data 바인딩하기
-    category.items.forEach((item) => {
-      const thumbnailTemplate = document.querySelector('#bestItemTemplate');
-      const thumbnailHTML = getThumbnailHTML(thumbnailTemplate, item);
-
-      thumbnailList.innerHTML += thumbnailHTML;
-    });
-
-    containerElement.appendChild(thumbnailList);
-  });
-
-  //4. tab 버튼 add click event
-  const thumbnailLists = document.querySelectorAll('.thumbnails');
-
-  tabElement.addEventListener('click', (evt) => {
-    const currentIndex = tabElement.dataset.currentIndex;
-    const selectedIndex = evt.target.dataset.index;
-
-    // button active toggle
-    tabElement.children[currentIndex].classList.remove('active');
-    evt.target.classList.add('active');
-
-    // ul display toggle
-    thumbnailLists[currentIndex].style = 'display: none;';
-    thumbnailLists[selectedIndex].style = 'display: block;';
-
-    tabElement.dataset.currentIndex = selectedIndex;
-  });
-
-  //5 초기화 (첫 번째 버튼에 active 클래스 추가 / 첫 번째 ul "display: block")
-  tabElement.children[0].classList.add('active');
-  thumbnailLists.forEach((thumb) => thumb.style = 'display: none;');
-  thumbnailLists[0].style = 'display: block;';
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   const BASE_URL = 'http://crong.codesquad.kr:8080/woowa/';
+  
+  const bestsellerTab = new TabMenu({
+    buttons: '.bestseller > .container > .tabs',
+    contents: '.bestseller > .container'
+  });
 
   requestData({
     url: BASE_URL + 'best',
-    callback: makeTabs
+    callback: bestsellerTab.init.bind(bestsellerTab)
   });
 });
