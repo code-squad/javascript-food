1a2f5e23551c893cad041bf313afebe2481aa4b3
shine1594 <panamera1594@gmail.com>
2017-11-28 12:01:19 +0900

    [IMP] implemented loading thumbnails by ajax request

--- js/main.js
+++ js/main.js
@@ -29,16 +29,79 @@ const getData = (() => {
 
 function makeTabs(response) {
   const jsonData = JSON.parse(response);
+  const containerElement = document.querySelector('.bestseller > .container');
+  const tabElement = document.querySelector('.bestseller .tabs');
+  const bestItemElement = document.querySelector('#bestItemTemplate');
 
-  jsonData.forEach((tabItem) => {
+  const makeListElement = (data) => {
+    const listElement = document.createElement('li');
+    listElement.dataset.index = data.index;
+    listElement.textContent = data.text;
 
+    return listElement;
+  };
+
+   const mappingThumbnailData = (template, obj) => {
+      const keys = Object.keys(obj);
+      let htmlStr = template.innerHTML;
+
+      keys.forEach((key) => {
+          htmlStr = htmlStr.replace(`{{${key}}}`, obj[key]);
+      });
+
+      htmlStr = htmlStr.replace('{{n_price}}', '');
+
+      const tempNode = document.createElement('div');
+      tempNode.innerHTML = htmlStr;
+
+
+      const deliveryInfo = tempNode.querySelector('.wrap-delivery-info');
+      const tags = tempNode.querySelector('.thumbnail__tags');
+
+      //delivery types
+      obj.delivery_type.forEach((type, index) => {
+        const info = document.createElement('sapn');
+        info.textContent = type;
+        
+        deliveryInfo.appendChild(info);
+      });
+
+      //tag
+      if (obj.badge) {
+        obj.badge.forEach((tagName, index) => {
+          const tag = document.createElement('li');
+          tag.textContent = tagName;
+          tags.appendChild(tag);
+        });
+      }
+
+      return tempNode.innerHTML;
+  };
+
+  jsonData.forEach((category, index) => {
     //1. tab 버튼 생성
+    const tabButton = makeListElement({
+      index: index,
+      text: category.name
+    });
 
     //2. ul 태그 생성
+    const thumbnailList = document.createElement('ul');
+    thumbnailList.classList.add('thumbnails');
+    thumbnailList.dataset.index = index;
     
     //3. thumbnail dom 생성후 data 바인딩하기
-    
+    category.items.forEach((item) => {
+      const thumbnailTemplate = document.querySelector('#bestItemTemplate');
+      const thumbnailHTML = mappingThumbnailData(thumbnailTemplate, item);
+
+      thumbnailList.innerHTML += thumbnailHTML;
+    });
+
+    containerElement.appendChild(thumbnailList);
+
     //4. tab 버튼 add click event
+
 
   });
 
