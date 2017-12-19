e7f11a91fcd4a304ba84bc2847c31b595ffa2eee
DESKTOP-MVLSK2J\Tim <gwanhyung787@gmail.com>
2017-12-19 16:36:48 +0900

    Add helpers.js

---
+++ helpers.js
@@ -0,0 +1,103 @@
+(function (window) {
+    'use strict';
+    /**
+     * querySelector wrapper
+     *
+     * @param {string} selector Selector to query
+     * @param {Element} [scope] Optional scope element for the selector
+     */
+    window.qs = function (selector, scope) {
+        return (scope || document).querySelector(selector);
+    };
+    window.qsa = function (selector, scope) {
+        return (scope || document).querySelectorAll(selector);
+    };
+
+    /**
+     * addEventListener wrapper
+     *
+     * @param {Element|Window} element Target Element
+     * @param {string} type Event name to bind to
+     * @param {Function} callback Event callback
+     * @param {boolean} useCapture Capture the event
+     */
+    window.$on = function (element, type, callback, useCapture) {
+        element.addEventListener(type, callback, useCapture);
+    };
+
+    /**
+     * Delegates event to a selector.
+     *
+     * @param {Element} element
+     * @param {String} selector
+     * @param {String} type
+     * @param {Function} callback
+     * @param {Boolean} useCapture
+     * @return {Object}
+     */
+    function _delegate(element, selector, type, callback, useCapture) {
+        var listenerFn = listener.apply(this, arguments);
+
+        element.addEventListener(type, listenerFn, useCapture);
+
+        return {
+            destroy: function () {
+                element.removeEventListener(type, listenerFn, useCapture);
+            }
+        }
+    }
+
+    /**
+     * Delegates event to a selector.
+     *
+     * @param {Element|String|Array} [elements]
+     * @param {String} selector
+     * @param {String} type
+     * @param {Function} callback
+     * @param {Boolean} useCapture
+     * @return {Object}
+     */
+    window.$delegate = function (elements, selector, type, callback, useCapture) {
+        // Handle the regular Element usage
+        if (typeof elements.addEventListener === 'function') {
+            return _delegate.apply(null, arguments);
+        }
+
+        // Handle Element-less usage, it defaults to global delegation
+        if (typeof type === 'function') {
+            // Use `document` as the first parameter, then apply arguments
+            // This is a short way to .unshift `arguments` without running into deoptimizations
+            return _delegate.bind(null, document).apply(null, arguments);
+        }
+
+        // Handle Selector-based usage
+        if (typeof elements === 'string') {
+            elements = document.querySelectorAll(elements);
+        }
+
+        // Handle Array-like based usage
+        return Array.prototype.map.call(elements, function (element) {
+            return _delegate(element, selector, type, callback, useCapture);
+        });
+    }
+
+    /**
+     * Finds closest match and invokes callback.
+     *
+     * @param {Element} element
+     * @param {String} selector
+     * @param {String} type
+     * @param {Function} callback
+     * @return {Function}
+     */
+    function listener(element, selector, type, callback) {
+        return function (e) {
+            e.delegateTarget = e.target.closest(selector);
+
+            if (e.delegateTarget) {
+                callback.call(element, e);
+            }
+        }
+    }
+
+})(window);
