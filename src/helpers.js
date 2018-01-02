599b92b0c1e5583232ff55bfc5ccc8094fb30908
DESKTOP-MVLSK2J\Tim <gwanhyung787@gmail.com>
2018-01-02 11:04:57 +0900

    Implement throttle function

--- src/helpers.js
+++ src/helpers.js
@@ -119,29 +119,19 @@ export function request(url) {
  * Returns a new function that, when invoked, invokes `func` at most once per `wait` milliseconds.
  *
  * @param {Function} func Function to wrap.
- * @param {Number} wait Number of milliseconds that must elapse between `func` invocations.
+ * @param {Number} limit Number of milliseconds that must elapse between `func` invocations.
  * @return {Function} A new function that wraps the `func` function passed in.
  */
 
-export function throttle(func, wait) {
-    var ctx, args, rtn, timeoutID; // caching
-    var last = 0;
-
-    return function throttled() {
-        ctx = this;
-        args = arguments;
-        var delta = new Date() - last;
-        if (!timeoutID)
-            if (delta >= wait) call();
-            else timeoutID = setTimeout(call, wait - delta);
-        return rtn;
+export function throttle(func, limit) {
+    let wait = false;
+    return () => {
+        if (!wait) {
+            func.apply(null, arguments);
+            wait = true;
+            setTimeout(() => {
+                wait = false;
+            }, limit);
+        }
     };
-
-    function call() {
-        timeoutID = 0;
-        last = +new Date();
-        rtn = func.apply(ctx, args);
-        ctx = null;
-        args = null;
-    }
 }
