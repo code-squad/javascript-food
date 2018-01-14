8c6980f81b6c1e1ce73b42ecbcd7193fc7a71b9a
DESKTOP-MVLSK2J\Tim <gwanhyung787@gmail.com>
2018-01-14 23:37:16 +0900

    Remove server

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-app.listen(3000, function() {
-     console.log("Start server on port 3000!");
-});
-app.use(express.static('dist'));
-app.get('/', function (req, res, next) {
-    res.sendFile(__dirname+'/dist/index.html');
-});
