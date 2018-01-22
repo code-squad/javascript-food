38d822325eb5fd4aae460b11f754f9bd2042ade6
DESKTOP-MVLSK2J\Tim <gwanhyung787@gmail.com>
2018-01-22 19:26:43 +0900

    Add Scroller bind hide

--- src/view/ScrollerView.js
+++ src/view/ScrollerView.js
@@ -1,14 +1,24 @@
 import {
+    on,
+    qs,
     delegate
 } from '../helpers';
 
 export default class {
+    constructor() {
+        this.updownButton = qs('.page_up_down_list');
+    }
 
     bind(bindCmd, handler) {
         const bindCommands = {
             click: () => {
-                delegate('.page_up_down_list', '.page_up_down_list > li > a',
+                delegate(this.updownButton, '.page_up_down_list > li > a',
                     'click', e => handler(e.delegateTarget.dataset.direction));
+            },
+            hide: () => {
+                on(window, 'scroll', () => {
+                    this.updownButton.style.display = window.scrollY > 90 ? 'block' : 'none';
+                });
             }
         };
 
