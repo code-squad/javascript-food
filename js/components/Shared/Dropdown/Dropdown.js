0ed6ad486dcfea2611ce6b10e0c4c29585f93a92
chany <lyhy0310@gmail.com>
2018-07-13 21:17:01 +0900

    [add] dropdown toggle

--- js/components/Shared/Dropdown/Dropdown.js
+++ js/components/Shared/Dropdown/Dropdown.js
@@ -3,9 +3,19 @@ import { qs } from '../../../helper/helper.js';
 export default class Dropdown {
   constructor(selector) {
     this.dropdwonEl = qs(selector);
+    this.showState = false
   }
 
   render(template, data) {
     this.dropdwonEl.innerHTML = template(data);
   }
+  toggleShowState(){
+    this.showState = !this.showState;
+    this.setDisplay()
+  }
+  setDisplay(){
+    if(this.showState) this.dropdwonEl.style.display = "block"
+    else this.dropdwonEl.style.display = ""
+  }
+
 }
