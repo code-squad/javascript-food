e76750c07d8da33a50a92ca8b7c04c624d5d97ad
chany <lyhy0310@gmail.com>
2018-07-17 15:30:56 +0900

    tab init

---
+++ js/components/Shared/Tab/Tab.js
@@ -0,0 +1,10 @@
+import { qs } from '../../../helper/helper.js';
+export default class Tab {
+  constructor(selector) {
+    this.TabButtonsEl = qs(selector);
+  }
+
+  render(template, data) {
+    this.TabButtonsEl.innerHTML = template(data);
+  }
+}
