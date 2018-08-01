987087c637f01e83293ffbc986c299f08396e509
youngdo212 <nsa06031@gmail.com>
2018-08-01 21:35:11 +0900

    refactor: template 모듈 분리

--- js/index.js
+++ js/index.js
@@ -1,30 +1,7 @@
 import {Model} from "./model.js";
 import {Controller} from "./controller.js";
 import {MenuNavigation} from "./menuNavigation.js";
-
-class Template {
-  menuLayer({name, subMenuData}) {
-    return this._menuLayerUpper(name) + this._menuLayerLower(subMenuData);
-  }
-
-  menu(name) {
-    return `<span>${name}</span>`;
-  }
-
-  _menuLayerUpper(name) {
-    return `<div class='menu_layer_upper'>${name}</div>`
-  }
-
-  _menuLayerLower(subMenuData) {
-    let subMenuListItems = '';
-
-    subMenuData.forEach(subMenu => {
-      subMenuListItems += `<li class='sub_menu'>${subMenu}</li>`
-    })
-
-    return `<ul class='menu_layer_lower'>${subMenuListItems}</ul>`;
-  }
-}
+import {Template} from './template.js';
 
 let template = new Template();
 
