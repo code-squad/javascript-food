85abbcf9ee4b5cbe2a23972aaaaf0d56b3ecdfe0
chany <lyhy0310@gmail.com>
2018-08-03 15:25:20 +0900

    [add] animation function add

--- js/helper/animation/raf.js
+++ js/helper/animation/raf.js
@@ -1,4 +1,30 @@
-export const animation = ((window)=>{
-  return (callback)=>window.requestAnimationFrame(callback);
-})(window);
+export const animation = (window => callback => window.requestAnimationFrame(callback))(window);
+
+
+if (!window.requestAnimationFrame) {
+  window. requestAnimationFrame = function (fn) {
+      var timer = 16.66; // 60 fps
+      setTimeout(fn,timer);
+  }
+}
+else {
+  const ani = {
+    fadeOut(el) {
+      let opacity = 1;
+      function decrease() {
+        opacity -= 0.05;
+        if (opacity <= 0) {
+          // complete
+          element.style.opacity = 0;
+          return true;
+        }
+        element.style.opacity = opacity;
+        requestAnimationFrame(decrease);
+      }
+      decrease();
+    },
+  };
+    
+}
+
 
