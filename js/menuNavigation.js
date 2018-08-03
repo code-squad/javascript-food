6ff12ccef475522f2c1eaff7102310cffd6343fa
youngdo212 <nsa06031@gmail.com>
2018-08-03 17:02:16 +0900

    style: 엘리먼트 이름규칙 변경 e > el

--- js/menuNavigation.js
+++ js/menuNavigation.js
@@ -1,12 +1,12 @@
 export let MenuNavigation = (function() {
   let MenuNavigation = function({menuNavigation, template}) {
-    this.eMenuNavigation = menuNavigation;
+    this.elMenuNavigation = menuNavigation;
     this.oTemplate = template;
   }
 
   MenuNavigation.prototype = {
     render(menuData){
-      this.eMenuNavigation.innerHTML = this.oTemplate.menuNavigation(menuData);
+      this.elMenuNavigation.innerHTML = this.oTemplate.menuNavigation(menuData);
     }
   }
 
