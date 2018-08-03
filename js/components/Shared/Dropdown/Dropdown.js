8758bcb881f5e8152d96766a8a9b188ef57a71f3
chany <lyhy0310@gmail.com>
2018-08-03 18:30:07 +0900

    [fix] dropdown js로 스타일 조작 -> class로 수정

--- js/components/Shared/Dropdown/Dropdown.js
+++ js/components/Shared/Dropdown/Dropdown.js
@@ -1,4 +1,4 @@
-import { qs, $on } from "../../../helper/helper.js";
+import { qs, $on } from '../../../helper/helper.js';
 
 export default class Dropdown {
   constructor(dropdownSelector, triggerSelector, dropdwonController) {
@@ -7,12 +7,12 @@ export default class Dropdown {
     this.dropdwonController = dropdwonController;
   }
   bindEvents(el) {
-    $on(el, "click", () => this.dropdwonController.subscribe(this.dropdwonEl));
-    $on(el, "click", this.toggleDisplay.bind(this));
+    $on(el, 'click', () => this.dropdwonController.subscribe(this.dropdwonEl));
+    $on(el, 'click', this.toggleDisplay.bind(this));
   }
-  toggleDisplay() {   
-    const isblock = this.dropdwonEl.style.display === "block";
-    if (isblock) this.dropdwonEl.style.display = "none";
-    else this.dropdwonEl.style.display = "block";
+  toggleDisplay() {
+    const isblock = this.dropdwonEl.className === 'dropdown show';
+    if (isblock) this.dropdwonEl.classList.remove('show');
+    else this.dropdwonEl.classList.add('show');
   }
 }
--- js/components/Shared/Dropdown/DropdownController.js
+++ js/components/Shared/Dropdown/DropdownController.js
@@ -1,10 +1,11 @@
 export default class DropdownController {
-  constructor(){
+  constructor() {
     this.activeDropdown = null;
   }
-  subscribe(dropdwonEl){    
-    if(!this.activeDropdown || this.activeDropdown === dropdwonEl) return this.activeDropdown = dropdwonEl;
-    else this.activeDropdown.style.display = 'none';
-    this.activeDropdown = dropdwonEl
+  subscribe(dropdwonEl) {    
+  
+    if (!this.activeDropdown || this.activeDropdown === dropdwonEl) return this.activeDropdown = dropdwonEl;
+    this.activeDropdown.classList.remove('show');
+    this.activeDropdown = dropdwonEl;
   }
 }
