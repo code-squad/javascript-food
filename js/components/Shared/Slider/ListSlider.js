c7b683d9293629abed5612775735caa2de663d13
chany <lyhy0310@gmail.com>
2018-08-07 19:37:29 +0900

    [add] 무한슬라이딩 draft add

--- js/components/Shared/Slider/ListSlider.js
+++ js/components/Shared/Slider/ListSlider.js
@@ -2,7 +2,7 @@ import { qs, $on } from '../../../helper/helper.js';
 import { cardTemplate, slidEButtonTemplate } from './template/template.js';
 import animations from '../../../helper/animation/raf.js';
 // import { mockData } from '../../../../assets/data/mainSlide.js';
-
+const initPosition = -980;
 export default class ListSlider {
   constructor(slideSelector, dataHelper, url ) {
     this.slideEl = qs(slideSelector);
@@ -10,7 +10,7 @@ export default class ListSlider {
     this.dataHelper = dataHelper;
     this.maxIdx = null;
     this.currentIdx = 0;
-    this.position = 0;
+    this.position = initPosition;
     this.init();
   }
   init(){
@@ -23,32 +23,53 @@ export default class ListSlider {
   getData(data){
     this.renderSlides(data);
   }
+  makeFakeData(data2){
+    this.slideEl.insertAdjacentHTML('afterbegin', cardTemplate(data2.slice((this.maxIdx)*4)));
+    this.slideEl.insertAdjacentHTML('beforeend', cardTemplate(data2.slice(0,4)));
+  }
   renderSlides(data){
-    this.setMaxIdx(data.length)
-    this.slideEl.innerHTML = cardTemplate(data);
+    const data2 = [...data, ...data]
+    this.setMaxIdx(data2.length)
+    this.slideEl.innerHTML = cardTemplate(data2);
+    // animation을 위한 fakedata // 무한 롤링을 주기 위해서 first, last를 같 끝에 추가해줬습니다 .
+    this.makeFakeData(data2);
+    // renderButtons
     this.slideEl.parentElement.insertAdjacentHTML('afterend', slidEButtonTemplate);
+    this.slideEl.style.transform = `translateX(${this.setPosition(this.currentIdx)}px)`;
     this.bindEvents();
-    this.slideEl.style.transition = 'all 0.5s ease';
   }
   bindEvents(){
     const slideButtonList = this.slideEl.parentElement.nextElementSibling;
     $on(qs('.right-button', slideButtonList), 'click', this.handleRightButtonClicked.bind(this));
     $on(qs('.left-button', slideButtonList), 'click', this.handleLeftButtonClicked.bind(this));
+    $on(this.slideEl, 'transitionend', this.handleEdgeSlide.bind(this))
   }
   setPosition(idx){
-    return this.position = idx*(-980);
+    return this.position = initPosition+ idx*(initPosition);
   }
   setCurrentIdx(idx){
     return this.currentIdx = idx;
   }
+  isEdgeSlide(){
+    return !!(this.currentIdx===-1 || this.currentIdx===this.maxIdx+1);
+  }
+  handleEdgeSlide(){
+      if(!this.isEdgeSlide()) return;
+      this.slideEl.style.transitionDuration = "0s";
+      const idx = this.currentIdx===-1 ? this.maxIdx : 0
+      this.setCurrentIdx(idx);
+      this.slideEl.style.transform = `translateX(${this.setPosition(this.currentIdx)}px)`
+  }
   handleRightButtonClicked(e){
-    this.setCurrentIdx(this.currentIdx+1)
-    if(this.currentIdx > this.maxIdx) this.setCurrentIdx(0)
-    this.slideEl.style.transform = `translateX(${this.setPosition(this.currentIdx)}px)`;
+      this.slideEl.style.transitionDuration = "0.5s";
+      this.setCurrentIdx(this.currentIdx+1)
+      this.slideEl.style.transform = `translateX(${this.setPosition(this.currentIdx)}px)`;
+      
   }
   handleLeftButtonClicked(e){
+    this.slideEl.style.transitionDuration = "0.5s";
     this.setCurrentIdx(this.currentIdx-1)
-    if(this.currentIdx < 0) this.setCurrentIdx(this.maxIdx)
     this.slideEl.style.transform = `translateX(${this.setPosition(this.currentIdx)}px)`;
+    if(this.isEdgeSlide()) $on(this.slideEl, 'transitionend', this.handleEdgeSlide.bind(this))
   }
 }
