744334236204519b528529590d54bdbea7b98e6c
chany <lyhy0310@gmail.com>
2018-08-08 19:42:19 +0900

    [fix] tabCard 가격 스타일 수정 and 이벤트 badge style 추가

--- js/components/Shared/Tab/tabTemplate.js
+++ js/components/Shared/Tab/tabTemplate.js
@@ -1,10 +1,16 @@
 
-export const badgeTemplate = badge =>
+export const handleBadgeTemplate = badge =>
 badge
-  ? `<div class="badge-box">
-<span>${badge}</span>
-</div>`
+  ? `${badgeTemplate(badge)}`
   : ``;
+
+export const badgeTemplate = data => 
+data.reduce(
+  (ac, c) =>
+    (ac += `<span>${c}</span>
+`),
+  ``
+);
 
 export const deliveryTemplate = data =>
 data.reduce(
@@ -14,6 +20,7 @@ data.reduce(
 `),
   ``
 );
+export const handleUndefined = data => data ? data : ""
 
 export const tabCardTemplate = data =>
 data.reduce(
@@ -29,7 +36,9 @@ data.reduce(
           ${deliveryTemplate(c.delivery_type)}
         </ul>
       </div>
-      ${badgeTemplate(c.badge)}
+      <div class="badge-box">
+        ${handleBadgeTemplate(c.badge)}
+      </div>
     </div>
     <div class="tab-card__content-box">
       <h3 class="title">${c.title}</h3>
@@ -38,7 +47,8 @@ data.reduce(
         <span></span>
       </div>
       <div class="price-box">
-        <span>${c.s_price}</span>
+        <span class="special-price">${c.s_price}</span>
+        <span class="normal-price">${handleUndefined(c.n_price)}</span>
       </div>
     </div>
   </div>
@@ -46,3 +56,28 @@ data.reduce(
 </li>`),
   ""
 );
+
+
+// alt"[소중한식사] 경상도 한상차림"
+// badge ["이벤트특가"]
+// delivery_type
+// :
+// (2) ["새벽배송", "전국택배"]
+// description
+// :
+// "경상도 명물 요리 세 가지를 한 상에!"
+// detail_hash
+// :
+// "H9881"
+// image
+// :
+// "https://cdn.bmf.kr/_data/product/H9881/f2a7b4df359c850b1a9eb57e17ddf6fc.jpg"
+// n_price
+// :
+// "39,000"
+// s_price
+// :
+// "31,200원"
+// title
+// :
+// "[소중한식사] 경상도 한상차림"
