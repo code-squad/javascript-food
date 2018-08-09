ff5cd6d8f9ed6f9f541ea9b2ee9d42d0f16d9a94
chany <lyhy0310@gmail.com>
2018-08-09 18:16:35 +0900

    [fix] refactoring naming

--- js/components/Shared/Slider/ListSlider.js
+++ js/components/Shared/Slider/ListSlider.js
@@ -1,7 +1,6 @@
 import { qs, $on } from '../../../helper/helper.js';
 import { cardTemplate, slidEButtonTemplate, padTemplate } from './template/template.js';
 import animations from '../../../helper/animation/raf.js';
-// import { mockData } from '../../../../assets/data/mainSlide.js';
 
 export default class ListSlider {
   constructor(slideSelector, dataHelper, url, initPosition = -980, listItemCounts = 4 ) {
@@ -50,14 +49,6 @@ export default class ListSlider {
     else return this.padElCounts = this.listItemCounts - restSlides;
   }
   renderSlides(slideData){
-<<<<<<< HEAD
-=======
-    // changeLength data변경을 위한 단순 test용
-    
-    // const changeLength = 5;
-    // const slideData = [...data, ...data.slice(changeLength)]
-
->>>>>>> b5be8138a213c591b7e1dc36836a900c4d111b13
     const slidesCounts = slideData.length;
     this.setMaxIdx(slidesCounts);
     this.renderRealSlideData(slideData);
@@ -75,15 +66,6 @@ export default class ListSlider {
   amendFakeEdgeData(slidesCounts, slideData){
     this.setPadCounts(slidesCounts)
     this.makeEdgeData(slideData);
-<<<<<<< HEAD
-=======
-    // renderButtons
-    this.renderButtons();
-
-    this.slideEl.style.transform = `translateX(${this.setPosition(this.currentIdx)}px)`;
-    
-    this.bindEvents();
->>>>>>> b5be8138a213c591b7e1dc36836a900c4d111b13
   }
   bindEvents(){
     this.slideButtonList = this.slideEl.parentElement.nextElementSibling;
