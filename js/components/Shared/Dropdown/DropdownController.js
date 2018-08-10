087ad83065e5ee5c184afa18996a6bdd906d7f5e
chany <lyhy0310@gmail.com>
2018-08-10 20:36:16 +0900

    [fix] dropdown 과 dropdown controller class이름으로 구분 짓도록 수정

--- js/components/Shared/Dropdown/DropdownController.js
+++ js/components/Shared/Dropdown/DropdownController.js
@@ -5,7 +5,6 @@ export default class DropdownController {
   subscribe(dropdwonEl) {    
     if (!this.activeDropdown || this.activeDropdown === dropdwonEl) return this.activeDropdown = dropdwonEl;
     this.activeDropdown.classList.remove('show');
-    this.activeDropdown.dataset.show ='hide';
     this.activeDropdown = dropdwonEl;
   }
 }
