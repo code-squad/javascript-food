05ddeeaaf646653e0697e7db669f7e43647085d1
youngdo212 <nsa06031@gmail.com>
2018-08-21 22:49:53 +0900

    style: 주석제거

--- js/template.js
+++ js/template.js
@@ -115,23 +115,3 @@ export const Template = (function () {
   
   return Template;
 })();
-
-/*
-<li class='main_section_list_item'>
-  <div class="main_section_list_item_img_wrap">
-    <img class='main_section_list_item_img' src="./images/side_dish_1.jpg" alt="side_dish_1">
-    <div class="main_section_list_item_delivery_type_wrap">
-      <div class="main_section_list_item_delivery_type">
-        <div class="main_section_list_item_delivery_type_1">새벽배송</div>
-        <div class="main_section_list_item_delivery_type_2">전국택배</div>
-      </div>
-    </div>
-  </div>
-  <div class='main_section_list_item_title'>[집밥의 완성] 두부시래기조림 180g</div>
-  <div class='main_section_list_item_description'>칼칼해서 좋아요</div>
-  <div class='main_section_list_item_price'>
-    <span class='main_section_list_item_origin_price'>2,000</span><span class='main_section_list_item_sales_price'>1,200</span><span class="main_section_list_item_price_unit">원</span>
-  </div>
-  <div class='main_section_list_item_badge'>이벤트특가</div>
-</li>
-*/
