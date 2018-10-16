34fe601b8239b020c4760888924e1cc74894f327
imdonguk <com8157@naver.com>
2018-10-16 20:25:14 +0900

    Modify : index.js파일이 아닌 TabView에서 템플릿과 ajax함수 import

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
-import { ajax } from './util.js';
-import Template from './Template.js';
 import TabView from './TabView.js';
+import Transition from './Transition.js'
+
 
 const bestTabEl = document.querySelector('.best_seller_tab');
 const bestSellerURL = 'http://crong.codesquad.kr:8080/woowa/best/';
-const template = new Template();
 
-new TabView({
+const tabView = new TabView({
     tabElement: bestTabEl,
-    template: template.bestItemList.bind(template),
-    ajax: ajax,
     url: bestSellerURL
 });
+const transition = new Transition();
+
+function init() {
+    const body = document.querySelector('body');
+    body.addEventListener('click', (e) => {
+        if (e.target.tagName !== 'A') return;
+        e.preventDefault();
+    })
+    tabView.init();
+    transition.init();
+}
+
+init();
