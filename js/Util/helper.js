29e9672c2eb271cc1f24ee39d307a6b87de5b6aa
Log91 <peter1636@naver.com>
2018-10-22 14:59:22 +0900

    add: slides navigation 추가

--- js/Util/helper.js
+++ js/Util/helper.js
@@ -1,5 +1,8 @@
 export const qs = selector => document.querySelector(selector);
 export const qsa = selector => document.querySelectorAll(selector);
+export const $on = (targetNode, eventType, callback) => {
+  document.querySelector(targetNode).addEventListener(eventType, callback);
+}
 export const ajax = (url, handler, param1) => {
   const xhr = new XMLHttpRequest();
   xhr.addEventListener("load", () => {
--- js/View/Promotion.js
+++ js/View/Promotion.js
@@ -1,39 +1,62 @@
-import { qs, qsa } from '../Util/helper.js'
+import { qs, $on } from '../Util/helper.js'
 export default class Promotion {
   constructor() {
     this.index = 0;
   }
+
   initialize() {
     this.showSlide(this.index);
-    this.registEvt();
+    this._registEvt();
   }
-  registEvt() {
-    qs('.promotion_right_nav').addEventListener('click', (e) => {
-      e.preventDefault();
-      this.nextSlide();
-    })
-    qs('.promotion_left_nav').addEventListener('click', (e) => {
-      e.preventDefault();
-      this.prevSlide();
-    })
-  }
-  nextSlide() {
-    if (this.index >= qs('.promotion_ul').children.length - 1) return;
-    this.hideSlide(this.index);
-    this.index += 1;
-    this.showSlide(this.index);
-  }
-  prevSlide() {
-    if (!this.index) return;
-    this.hideSlide(this.index);
-    this.index -= 1;
-    this.showSlide(this.index);
-  }
-  showSlide(index) {
-    qs('.promotion_ul').children[index].classList.add('show');
 
+  _registEvt() {
+    $on('.promotion_right_nav', 'click', e => this.cbClickNextSlide(e));
+    $on('.promotion_left_nav', 'click', e => this.cbClickPrevSlide(e));
+    qs('.slides_navi').addEventListener('click', (e) => {
+      e.preventDefault();
+      if (e.target.tagName !== 'LI') return;
+      this.slidesNaviOn(e.target);
+      this.hideSlide();
+      this.index = +(e.target.dataset.index);
+      this.showSlide(this.index);
+    })
   }
-  hideSlide(index) {
-    qs('.promotion_ul').children[index].classList.remove('show');
+  cbClickNextSlide(event) {
+    event.preventDefault();
+    this.nextSlide();
+  }
+  cbClickPrevSlide(event) {
+    event.preventDefault();
+    this.prevSlide();
+  }
+
+  nextSlide() {
+    if (this.index >= qs('.promotion_ul').children.length - 1) this.index = 0;
+    else this.index += 1;
+    this.hideSlide();
+    this.showSlide(this.index);
+  }
+
+  slidesNaviOn(targetNode) {
+    if (qs('.slides_navi_li_clicked')) {
+      qs('.slides_navi_li_clicked').classList.remove('slides_navi_li_clicked')
+    }
+    targetNode.classList.add('slides_navi_li_clicked');
+  }
+
+  prevSlide() {
+    if (!this.index) this.index = qs('.promotion_ul').children.length - 1;
+    else this.index -= 1;
+    this.hideSlide();
+    this.showSlide(this.index);
+  }
+
+  showSlide(index) {
+    this.slidesNaviOn(qs(`[data-index='${index}']`));
+    qs('.promotion_ul').children[index].firstElementChild.classList.add('show');
+  }
+
+  hideSlide() {
+    qs('.show').classList.remove('show');
   }
 }
