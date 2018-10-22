17bf7d2b6ea2c11ed4cd320c48463cd751a4d7cf
Log91 <peter1636@naver.com>
2018-10-22 15:11:33 +0900

    refactor: promotion.js 중복 제거

--- js/View/Promotion.js
+++ js/View/Promotion.js
@@ -5,58 +5,56 @@ export default class Promotion {
   }
 
   initialize() {
-    this.showSlide(this.index);
-    this._registEvt();
+    this._showSlide(this.index);
+    this._registEvent();
   }
 
-  _registEvt() {
-    $on('.promotion_right_nav', 'click', e => this.cbClickNextSlide(e));
-    $on('.promotion_left_nav', 'click', e => this.cbClickPrevSlide(e));
-    qs('.slides_navi').addEventListener('click', (e) => {
-      e.preventDefault();
-      if (e.target.tagName !== 'LI') return;
-      this.slidesNaviOn(e.target);
-      this.hideSlide();
-      this.index = +(e.target.dataset.index);
-      this.showSlide(this.index);
-    })
-  }
-  cbClickNextSlide(event) {
-    event.preventDefault();
-    this.nextSlide();
-  }
-  cbClickPrevSlide(event) {
-    event.preventDefault();
-    this.prevSlide();
+  _registEvent() {
+    $on('.promotion_right_nav', 'click', e => this._cbClickNextSlide(e));
+    $on('.promotion_left_nav', 'click', e => this._cbClickPrevSlide(e));
+    $on('.slides_navi', 'click', e => this._cbClickSlidesNavi(e))
   }
 
-  nextSlide() {
+  _cbClickNextSlide(event) {
+    event.preventDefault();
+    this._nextSlide();
+  }
+
+  _cbClickPrevSlide(event) {
+    event.preventDefault();
+    this._prevSlide();
+  }
+
+  _cbClickSlidesNavi(event) {
+    event.preventDefault();
+    if (e.target.tagName !== 'LI') return;
+    this.slidesNaviOn(event.target);
+    this.index = +(e.target.dataset.index);
+    this._showSlide(this.index);
+  }
+
+  _nextSlide() {
     if (this.index >= qs('.promotion_ul').children.length - 1) this.index = 0;
     else this.index += 1;
-    this.hideSlide();
-    this.showSlide(this.index);
+    this._showSlide(this.index);
   }
 
-  slidesNaviOn(targetNode) {
+  _slidesNaviOn(targetNode) {
     if (qs('.slides_navi_li_clicked')) {
       qs('.slides_navi_li_clicked').classList.remove('slides_navi_li_clicked')
     }
     targetNode.classList.add('slides_navi_li_clicked');
   }
 
-  prevSlide() {
+  _prevSlide() {
     if (!this.index) this.index = qs('.promotion_ul').children.length - 1;
     else this.index -= 1;
-    this.hideSlide();
-    this.showSlide(this.index);
+    this._showSlide(this.index);
   }
 
-  showSlide(index) {
-    this.slidesNaviOn(qs(`[data-index='${index}']`));
+  _showSlide(index) {
+    if (qs('.show')) qs('.show').classList.remove('show');
+    this._slidesNaviOn(qs(`[data-index='${index}']`));
     qs('.promotion_ul').children[index].firstElementChild.classList.add('show');
-  }
-
-  hideSlide() {
-    qs('.show').classList.remove('show');
   }
 }
