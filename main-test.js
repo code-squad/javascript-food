3e7b15a8a28b479c7b786a9b541d76259fb66de9
HYEON(Mac) <antaehyeon@live.co.kr>
2018-10-23 16:21:20 +0900

    Refactor: 코드 구조 개선

--- main-test.js
+++ main-test.js
@@ -1,29 +1,77 @@
-const mainNaviElement = document.querySelector('.main_navi');
-const mainMenuElement = document.querySelector(".main_navi_list");
+function MenuLayoutManager(element) {
 
-let currentTarget;
-let currentParentElement;
-let currentListElement;
+    this.element = element;
 
-mainMenuElement.addEventListener('mouseover', (evt) => {
-    const target = evt.target;
-    if (target.nodeName === 'DIV') {
-        if (currentTarget && target != currentTarget) {
-            this.updateElementDisplayProperty(currentParentElement.lastElementChild, 'none');
-        }
+    this.currentElement = {
+        'target' : "",
+        'parent' : "",
+        'list' : "",
+    };
 
-        currentTarget = target;
-        currentParentElement = target.parentElement;
-        currentListElement = currentParentElement.lastElementChild;
-        
-        this.updateElementDisplayProperty(currentListElement, 'block');
-    }
-});
+    this.getCurrentElement = function() {
+        return this.currentElement;
+    };
 
-mainNaviElement.addEventListener('mouseleave', () => {
-    this.updateElementDisplayProperty(currentListElement, 'none');
-})
+    this.setCurrentElements = function(object) {
+        this.currentElement.target = object.target;
+        this.currentElement.parent = object.parent;
+        this.currentElement.list = object.list;
+    };
+
+}
+
+MenuLayoutManager.prototype = {
+
+    hover: function() {
+        this.mouseOver(this.element);
+        this.mouseLeave(this.element);
+    },
+
+    mouseOver: function(element) {
+        element.addEventListener('mouseover', (evt) => {
+            const target = evt.target;
+            this.showMenuAction(target);
+        });
+    },
+
+    mouseLeave: function(element) {
+        element.addEventListener('mouseleave', () => {
+            this.hideMenu(this.getCurrentElement().list);
+        });
+    },
+    
+    showMenuAction: function(element) {
+        if (element.nodeName != 'DIV') return;
+
+        const currentTarget = this.getCurrentElement().target;
+
+        if (currentTarget && element != currentTarget)
+            this.hideMenu(this.getCurrentElement().list);
+
+        this.setCurrentElements({
+            'target' : element,
+            'parent' : element.parentElement,
+            'list' : element.parentElement.lastElementChild,
+        });
+
+        this.showMenu(this.getCurrentElement().list);
+    }, 
+
+    showMenu: function(element) {        
+        updateElementDisplayProperty(element, 'block');
+    },
+
+    hideMenu: function(element) {
+        updateElementDisplayProperty(element, 'none');
+    },
+
+}
 
 function updateElementDisplayProperty(element, type) {
     element.style.display = type;
 }
+
+const mainNaviElement = document.querySelector('.main_navi'); 
+
+const menuLayoutManager = new MenuLayoutManager(mainNaviElement);
+menuLayoutManager.hover();
