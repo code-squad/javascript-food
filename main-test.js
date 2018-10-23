6765a32702de7a930f961a699f0e490fea7a33c1
HYEON(Mac) <antaehyeon@live.co.kr>
2018-10-23 17:18:14 +0900

    Refactor: 함수 분할 및 메뉴 하이라이트 기능 JS로 구현

--- main-test.js
+++ main-test.js
@@ -1,5 +1,4 @@
 function MenuLayoutManager(element) {
-
     this.element = element;
 
     this.currentElement = {
@@ -30,23 +29,27 @@ MenuLayoutManager.prototype = {
     mouseOver: function(element) {
         element.addEventListener('mouseover', (evt) => {
             const target = evt.target;
-            this.showMenuAction(target);
+            this.actionShowMainSubMenuList(target);
+            this.showMainMenuHighlight(this.getCurrentElement().target);
         });
     },
 
     mouseLeave: function(element) {
         element.addEventListener('mouseleave', () => {
-            this.hideMenu(this.getCurrentElement().list);
+            this.hideMainMenuSubList(this.getCurrentElement().list);
+            this.hideMainMenuHighlight(this.getCurrentElement().target);
         });
     },
     
-    showMenuAction: function(element) {
+    actionShowMainSubMenuList: function(element) {
         if (element.nodeName != 'DIV') return;
 
         const currentTarget = this.getCurrentElement().target;
 
-        if (currentTarget && element != currentTarget)
-            this.hideMenu(this.getCurrentElement().list);
+        if (currentTarget && element != currentTarget) {
+            this.hideMainMenuSubList(this.getCurrentElement().list);
+            this.hideMainMenuHighlight(currentTarget);
+        }
 
         this.setCurrentElements({
             'target' : element,
@@ -54,24 +57,49 @@ MenuLayoutManager.prototype = {
             'list' : element.parentElement.lastElementChild,
         });
 
-        this.showMenu(this.getCurrentElement().list);
-    }, 
-
-    showMenu: function(element) {        
-        updateElementDisplayProperty(element, 'block');
+        this.showMainMenuSubList(this.getCurrentElement().list);
     },
 
-    hideMenu: function(element) {
-        updateElementDisplayProperty(element, 'none');
+    showMainMenuHighlight: function(element) {
+        updateElementBackgroundColor(element, 'WHITE');
+        updateElementBorder(element, '1px solid BLACK');
+        updateElementTextColor(element.firstElementChild, "#2AC1BC");
     },
 
+    hideMainMenuHighlight: function(element) {
+        updateElementBackgroundColor(element, '#473F36');
+        updateElementBorder(element, 'NONE');
+        updateElementTextColor(element.firstElementChild, "WHITE");
+    },
+
+    showMainMenuSubList: function(element) {        
+        updateElementDisplayProperty(element, 'BLOCK');
+    },
+
+    hideMainMenuSubList: function(element) {
+        updateElementDisplayProperty(element, 'NONE');
+    },
+
+}
+
+function updateElementBackgroundColor(element, colorData) {
+    element.style.backgroundColor = colorData;
 }
 
 function updateElementDisplayProperty(element, type) {
     element.style.display = type;
 }
 
-const mainNaviElement = document.querySelector('.main_navi'); 
+function updateElementBorder(element, borderData) {
+    element.style.border = borderData;
+}
+
+function updateElementTextColor(element, colorData) {
+    element.style.color = colorData;
+}
+
+
+const mainNaviElement = document.querySelector('.main_navi');
 
 const menuLayoutManager = new MenuLayoutManager(mainNaviElement);
 menuLayoutManager.hover();
