222cdde69c74e732fd91c1c7d97a36dfd2c88fb8
Imdonguk <com8157@naver.com>
2018-10-29 23:20:12 +0900

    Modify : mainSlide구현할 때 썻던 클래서 subSlide에 재사용하여 구현

--- js/index.js
+++ js/index.js
@@ -8,12 +8,13 @@ const elements = {
     body: document.querySelector('body'),
     bestTabEl: document.querySelector('.best_seller_tab'),
     mainSlideEl: document.querySelector('.main_slide_wrap'),
-
+    subSlideEl: document.querySelector('.sub_slide_wrap')
 }
 
 const contentURL = {
     bestSeller: 'http://crong.codesquad.kr:8080/woowa/best/',
-    mainSlide: './jsonData/mainSlideData.json'
+    mainSlide: './jsonData/mainSlideData.json',
+    subSlide: './jsonData/subSlideData.json'
 }
 
 const tabView = new TabView({
@@ -23,16 +24,27 @@ const tabView = new TabView({
 
 const mainSlideContent = new SlideContent({
     slideListEl: elements.mainSlideEl.querySelector('.slide_list'),
-    url: contentURL.mainSlide,
-    runTime: 1000
+    url: contentURL.mainSlide
 })
 const mainSlideDots = new SlideDots({ dotListEl: elements.mainSlideEl.querySelector('.slide_dots') });
 const mainSlideNavi = new SlideNavi({ naviEl: elements.mainSlideEl.querySelector('.slides_navi') });
-const slideContoller = new SlideController({
+const mainSlideContoller = new SlideController({
     slideContent: mainSlideContent,
     slideNavi: mainSlideNavi,
     slideDots: mainSlideDots
 });
+
+const subSlideContent = new SlideContent({
+    slideListEl: elements.subSlideEl.querySelector('.slide_list'),
+    url: contentURL.subSlide
+})
+const subSlideDots = new SlideDots({ dotListEl: elements.subSlideEl.querySelector('.slide_dots')});
+const subSlideNavi = new SlideNavi({ naviEl : elements.subSlideEl.querySelector('.slides_navi')});
+const subSlideController = new SlideController({
+    slideContent: subSlideContent,
+    slideNavi: subSlideNavi,
+    slideDots: subSlideDots
+})
 
 
 function init() {
