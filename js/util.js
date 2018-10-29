3c719d67d95a45f1432fab7fdc5eea63772b6b13
Imdonguk <com8157@naver.com>
2018-10-29 23:07:14 +0900

    Add : subSlide Dom에 추가, 애니메이션 기능 transition으로 작동되게 변경

--- js/util.js
+++ js/util.js
@@ -9,32 +9,4 @@ function ajax({ url, handler, requestType }) {
     xhr.send();
 }
 
-
-function fadeIn(el, time) {
-    time = time / 1000;
-    let opacity = 0;
-    const fps = 1 / 60 / time;
-    function increaseOpacity() {
-        opacity += fps;
-        el.style.opacity = opacity;
-        if (opacity < 1) {
-            requestAnimationFrame(increaseOpacity);
-        }
-    }
-    requestAnimationFrame(increaseOpacity);
-}
-
-function fadeOut(el, time) {
-    time = time / 1000;
-    let opacity = 1;
-    const fps = 1 / 60 / time;
-    function decreaseOpacity() {
-        if (opacity < 0) return true;
-        opacity -= fps;
-        el.style.opacity = opacity;
-        requestAnimationFrame(decreaseOpacity);
-    }
-    requestAnimationFrame(decreaseOpacity);
-}
-
-export { ajax, fadeIn, fadeOut }
+export { ajax }
