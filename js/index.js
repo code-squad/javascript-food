f566998b2ae1f55ca76c8826e4e9b5af1bb8333f
Imdonguk <com8157@naver.com>
2018-11-09 01:08:48 +0900

    Add : 밑반찬, 국찌개, 일품요리 MenuSlide클래스를 재사용해서 추가UI구현

--- js/index.js
+++ js/index.js
@@ -3,18 +3,27 @@ import SlideContent from './view/SlideContent.js';
 import SlideDots from './view/SlideDots.js';
 import SlideNavi from './view/SlideNavi.js';
 import SlideController from './view/SlideController.js';
+import MenuSlide from './view/MenuSlide.js';
 
 const elements = {
     body: document.querySelector('body'),
     bestTabEl: document.querySelector('.best_seller_tab'),
     mainSlideEl: document.querySelector('.main_slide_wrap'),
-    subSlideEl: document.querySelector('.sub_slide_wrap')
+    subSlideEl: document.querySelector('.sub_slide_wrap'),
+    mainDishEl:document.querySelector(".dish_list_wrap[data-index='0']"),
+    sideDishEl:document.querySelector(".dish_list_wrap[data-index='1']"),
+    soupDishEl:document.querySelector(".dish_list_wrap[data-index='2']"),
+    courseDishEl:document.querySelector(".dish_list_wrap[data-index='3']")
 }
 
 const contentURL = {
     bestSeller: 'http://crong.codesquad.kr:8080/woowa/best/',
     mainSlide: './jsonData/mainSlideData.json',
-    subSlide: './jsonData/subSlideData.json'
+    subSlide: './jsonData/subSlideData.json',
+    mainDish: 'http://crong.codesquad.kr:8080/woowa/main',
+    slideDish: 'http://crong.codesquad.kr:8080/woowa/side',
+    soupDish: 'http://crong.codesquad.kr:8080/woowa/soup',
+    courseDish: 'http://crong.codesquad.kr:8080/woowa/course'
 }
 
 const tabView = new TabView({
@@ -46,6 +55,29 @@ const subSlideController = new SlideController({
     slideDots: subSlideDots
 })
 
+const mainDishSlide = new MenuSlide({
+    url : contentURL.mainDish,
+    slideListEl : elements.mainDishEl.querySelector('.slide_list'),
+    naviEl : elements.mainDishEl.querySelector('.slides_navi'),
+})
+
+const sideDishSlide = new MenuSlide({
+    url : contentURL.mainDish,
+    slideListEl : elements.sideDishEl.querySelector('.slide_list'),
+    naviEl : elements.sideDishEl.querySelector('.slides_navi'),
+})
+
+const soupDishSlide = new MenuSlide({
+    url : contentURL.soupDish,
+    slideListEl : elements.soupDishEl.querySelector('.slide_list'),
+    naviEl : elements.soupDishEl.querySelector('.slides_navi'),
+})
+
+const courseDishSlide = new MenuSlide({
+    url : contentURL.courseDish,
+    slideListEl : elements.courseDishEl.querySelector('.slide_list'),
+    naviEl : elements.courseDishEl.querySelector('.slides_navi'),
+})
 
 function init() {
     elements.body.addEventListener('click', (e) => {
