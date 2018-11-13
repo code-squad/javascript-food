385bc878d8599adf569f5a00871bedae7cbfc40d
Imdonguk <com8157@naver.com>
2018-11-13 20:46:46 +0900

    Add : scroll할 때에는 버튼이 안보이게 하고 일정시간동안 scroll하지 않을 시에 보이게하는 viewScrollBtn메서드추가

--- js/view/ScrollBtnView.js
+++ js/view/ScrollBtnView.js
@@ -1,12 +1,26 @@
-export default class Scroll{
-    constructor({scrollEl}){
+export default class ScrollBtnView{
+    constructor({scrollEl, debounceTimer = 200}){
         this.scrollEl = scrollEl;
         this.clickScrollBtn();
+        this.viewScrollBtn(debounceTimer);
     }
     
     clickScrollBtn(){ 
         this.scrollEl.addEventListener('click', ({target})=>{
             this.pageScroll(target.className);
+        })
+    }
+
+    viewScrollBtn(timer){
+        let shutter;
+        window.addEventListener('scroll', ()=>{
+
+            clearTimeout(shutter);
+            this.scrollEl.style.opacity = "0";
+
+            shutter = setTimeout(()=>{
+                this.scrollEl.style.opacity = "1";
+            },timer)
         })
     }
 
