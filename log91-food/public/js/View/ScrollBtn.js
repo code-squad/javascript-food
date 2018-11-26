7d54af19ad44078f0d2900ec896a4716cd60c5be
Log91 <peter1636@naver.com>
2018-11-26 16:52:52 +0900

    update: 자동완성 구현까지만 완료
    1. 리팩토링 필요
    2. Arrowdown키 이벤트가 최초발생 시 두번째 목록부터 시작하는 버그있음

--- log91-food/public/js/View/ScrollBtn.js
+++ log91-food/public/js/View/ScrollBtn.js
@@ -20,11 +20,32 @@ export default class ScrollBtn {
     scrollNode.innerHTML = this.scrollBtnTpl;
     this.setCoordiantes(scrollNode, this.leftValue, this.topValue);
     document.body.appendChild(scrollNode);
-    document.querySelector('.search_bar>ul').firstElementChild.focus();
-    $on(qs('.search_bar_in'), 'keydown', e => {
+    // document.querySelector('.search_bar>ul').firstElementChild.focus();
+    $on(qs('.search_wrap'), 'keydown', e => {
+      if (!qs('.search_auto_drop')) return;
       if (e.code === 'ArrowDown') {
-        if (qs('.search_auto_drop'))
-          qs('.search_auto_drop').firstElementChild.classList.add('search_auto_drop_focus');
+        if (!qs('.search_auto_drop_hl')) {
+          qs('.search_auto_list').firstElementChild.classList.add('search_auto_drop_hl');
+          // debugger;
+          console.log('wfewfwef');
+          return;
+        }
+        // debugger;
+        if (!qs('.search_auto_drop_hl').nextElementSibling) return;
+        qs('.search_auto_drop_hl').nextElementSibling.classList.add('search_auto_drop_hl');
+        qsa('.search_auto_drop_hl')[0].classList.remove('search_auto_drop_hl');
+      }
+      else if (e.code === 'ArrowUp') {
+        if (!qs('.search_auto_drop_hl').previousElementSibling) return;
+        console.log(qs('.search_auto_drop_hl').previousElementSibling.classList);
+        qs('.search_auto_drop_hl').previousElementSibling.classList.add('search_auto_drop_hl');
+        qsa('.search_auto_drop_hl')[1].classList.remove('search_auto_drop_hl')
+      }
+      else if (e.code === "Enter") {
+        if (!qs('.search_auto_drop_hl')) return;
+        const completed = qs('.search_auto_drop_hl').firstElementChild.innerHTML.replace(/\<strong\>|\<\/strong\>/g, '');
+        qs('.search_bar_in').value = completed;
+        qs('.search_auto_list').innerHTML = '';
       }
       return;
     });
@@ -37,9 +58,11 @@ export default class ScrollBtn {
             document.querySelector('.search_bar>ul').innerHTML = '';
             return;
           }
-          console.log(JSON.parse(response));
-          document.querySelector('.search_bar>ul').innerHTML = '';
-          JSON.parse(response)[1].forEach(v => {
+          // document.querySelector('.search_bar>ul').innerHTML = '';
+          const parsedData = JSON.parse(response);
+          parsedData[1].forEach(v => {
+            v = v[0].replace(parsedData[0], `<strong>${parsedData[0]}</strong>`);
+            // console.log(v.replace(response, 'hello'));
             document.querySelector('.search_bar>ul').innerHTML += `
             <div class="search_auto_drop"><span class="search_auto_drop_sp">${v}</span></div>
             `
