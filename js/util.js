d22244e1b2a5c9e7a1a90dd8e828f735fa580375
Imdonguk <com8157@naver.com>
2018-12-01 18:25:48 +0900

    Add : 6시간이 지나면 로컬데이터를 지우는 함수 추가

--- js/util.js
+++ js/util.js
@@ -1,42 +1,49 @@
 function ajax({ url, handler, requestType }) {
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', () => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => {
 
-    const requestData = JSON.parse(xhr.response);
-    handler(requestData);
-  })
-  xhr.open(requestType, url);
-  xhr.send();
+        const requestData = JSON.parse(xhr.response);
+        handler(requestData);
+    })
+    xhr.open(requestType, url);
+    xhr.send();
 }
 
 function throttle(func, timer) {
-  let shutter;
-  return function () {
-    if (!shutter) {
-      func.apply(null, arguments);
-      shutter = setTimeout(() => shutter = null, timer);
+    let shutter;
+    return function () {
+        if (!shutter) {
+            func.apply(null, arguments);
+            shutter = setTimeout(() => shutter = null, timer);
+        }
     }
-  }
 }
 
 function debounce(func, timer) {
-  let shutter;
-  return function () {
-    clearTimeout(shutter);
-    shutter = setTimeout(() => { func.apply(null, arguments) }, timer);
-  }
+    let shutter;
+    return function () {
+        clearTimeout(shutter);
+        shutter = setTimeout(() => { func.apply(null, arguments) }, timer);
+    }
 }
 
 function hideElement(element) {
-  element.classList.remove('show');
-  element.classList.add('hide');
+    element.classList.remove('show');
+    element.classList.add('hide');
 }
 
 function showElement(element) {
-  element.classList.remove('hide');
-  element.classList.add('show');
+    element.classList.remove('hide');
+    element.classList.add('show');
 }
 
 const pipe = (...fns) => value => fns.reduce((acc, fn) => fn(acc), value);
 
-export { ajax, throttle, debounce, hideElement, showElement, pipe }
+function removeExpirationItems() {
+    Object.keys(localStorage).forEach(v => {
+        const expieationTime = 1000 * 60 * 60; //6시간
+        if (new Date() - JSON.parse(localStorage[v]).time > expieationTime) localStorage.removeItem(v);
+    })
+}
+
+export { ajax, throttle, debounce, hideElement, showElement, pipe, removeExpirationItems }
