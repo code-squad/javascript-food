0607d2e0d91ceb402f4eafb0f423949ab2411332
Imdonguk <com8157@naver.com>
2018-12-02 17:55:31 +0900

    Add : 최근검색어 template추가

--- js/template/searchListTpl.js
+++ js/template/searchListTpl.js
@@ -6,11 +6,13 @@ function relatedQueryTpl(inputvalue, relatedValue) {
     return `<li class="relatedValue">${relatedValue}</li>`.replace(RegExp(inputvalue, 'g'), highlightElmentTpl(inputvalue))
 }
 
-function searchListTpl(searchListData) {
+export function searchListTpl(searchListData) {
     const maxContentCount = 10;
     let [inputValue, relatedValues] = searchListData;
     relatedValues = relatedValues.length > maxContentCount ? relatedValues.slice(0, maxContentCount) : relatedValues;
     return relatedValues.reduce((prev, curr) => prev.concat(relatedQueryTpl(inputValue, curr)), '');
 }
 
-export { searchListTpl }
+export function recentListTpl(recentList) {
+    return recentList.reduce((prev, curr) => prev + `<li class="relatedValue">${curr}</li>`, '')
+}
